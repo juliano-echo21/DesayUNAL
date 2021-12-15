f35ae1d741356ef883118cab0a7615b5e1409d66
Julian ro <davidro2121@gmail.com>
2021-12-15 10:39:12 -0500

    first commit

---
+++ src/main/java/com/example/desayunal/DesayUnalApplication.java
@@ -0,0 +1,13 @@
+package com.example.desayunal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesayUnalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesayUnalApplication.class, args);
+    }
+
+}
