cb506653355ada7572e9652c7798bc16ef4b8b9c
AndersonVargas8 <anvargasa@unal.edu.co>
2021-12-30 14:17:25 -0500

    Se completaron los atributos de Producto tanto en la clase como en el html

--- src/main/java/com/example/desayunal/model/Producto.java
+++ src/main/java/com/example/desayunal/model/Producto.java
@@ -9,17 +9,23 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
+    private String descripcion;
+    private String estado;
     private int precio;
+    private String categoria;
 
 
     public Producto(){
 
     }
-    public Producto(int id, String nombre, int precio) {
-        super();
+
+    public Producto(int id, String nombre, String descripcion, String estado, int precio, String categoria) {
         this.id = id;
         this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
         this.precio = precio;
+        this.categoria = categoria;
     }
 
     public int getId() {
@@ -44,5 +50,29 @@ public class Producto {
 
     public void setPrecio(int precio) {
         this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
     }
 }
