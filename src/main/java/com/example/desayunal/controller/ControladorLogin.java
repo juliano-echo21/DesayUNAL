51686d1943c615457565578f32d37173c5bd535d
Carlixtos <carlosandresrio33@hotmail.com>
2022-01-12 09:42:42 -0500

    Se modificó el template de signUp y se terminó el controlador del login

--- src/main/java/com/example/desayunal/controller/ControladorLogin.java
+++ src/main/java/com/example/desayunal/controller/ControladorLogin.java
@@ -1,10 +1,14 @@
 package com.example.desayunal.controller;
 
 import com.example.desayunal.services.ServicioUsuario;
+import com.example.desayunal.web.dto.RegistroUsuarioDto;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,8 +18,29 @@ public class ControladorLogin {
     @Autowired
     private ServicioUsuario servicio;
 
+    @ModelAttribute("Usuario")
+    public RegistroUsuarioDto registroUsuarioDto(){
+
+        return new RegistroUsuarioDto();
+    }
+
     @GetMapping
-    public String loggear(String userName, String password, Model model) {
+    public String showLogin(String userName, String password, Model model) {
         return "login";
     }
+
+    @PostMapping
+    public String loggear(@ModelAttribute("Usuario")RegistroUsuarioDto registroDto){
+        String realPassword =  servicio.userPassword(registroDto.getUserName());
+        
+        if(realPassword != null)
+        {
+            if(realPassword.equals(registroDto.getPassword())) return "catalogo";
+
+            return "login?failure"; // No existe el usuario
+        }
+        
+        return "login?failure"; // No coinciden el usuario y la contraseña
+    }
+
 }
