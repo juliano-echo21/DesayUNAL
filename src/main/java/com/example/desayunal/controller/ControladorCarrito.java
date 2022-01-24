80c14e527c06299032ef52812ea477ba751eb3e2
AndersonVargas8 <anvargasa@unal.edu.co>
2022-01-24 17:51:29 -0500

    Se agregan las opciones de eliminar y editar en el carrito

--- src/main/java/com/example/desayunal/controller/ControladorCarrito.java
+++ src/main/java/com/example/desayunal/controller/ControladorCarrito.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
@@ -31,21 +33,43 @@ public class ControladorCarrito {
     
     @GetMapping("/agregarCarrito/{id}")
     public String agregarCarrito( @PathVariable int id, Model model){
- 
-
+        int pos = 0;
+        cantidad = 1;
         Producto p = sProducto.listarId(id).get();
 
-        Carrito car = new Carrito();
-        car.setId(++item);
-        car.setIdProducto(id);
-        car.setNombre(p.getNombre());
-        car.setDescripcion(p.getDescripcion());
-        car.setPrecioCompra(p.getPrecio());
-        car.setCantidad(cantidad);
-        car.setSubTotal(cantidad*p.getPrecio());
-
-        listaCarrito.add(car);
-        
+        if(!listaCarrito.empty()){
+            for(int i = 0; i < listaCarrito.size(); i++){
+                if(id == listaCarrito.get(i).getIdProducto()){
+                    pos = i;
+                }
+            }
+            if(id == listaCarrito.get(pos).getIdProducto()){
+                cantidad += listaCarrito.get(pos).getCantidad();
+                int subTotal = listaCarrito.get(pos).getPrecioCompra()*cantidad;
+                listaCarrito.get(pos).setCantidad(cantidad);
+                listaCarrito.get(pos).setSubTotal(subTotal);
+            }else{
+                Carrito car = new Carrito();
+                car.setId(++item);
+                car.setIdProducto(id);
+                car.setNombre(p.getNombre());
+                car.setDescripcion(p.getDescripcion());
+                car.setPrecioCompra(p.getPrecio());
+                car.setCantidad(cantidad);
+                car.setSubTotal(cantidad*p.getPrecio());
+                listaCarrito.add(car);
+            }
+        }else{
+            Carrito car = new Carrito();
+            car.setId(++item);
+            car.setIdProducto(id);
+            car.setNombre(p.getNombre());
+            car.setDescripcion(p.getDescripcion());
+            car.setPrecioCompra(p.getPrecio());
+            car.setCantidad(cantidad);
+            car.setSubTotal(cantidad*p.getPrecio());
+            listaCarrito.add(car);
+        }
         cantidadCarrito = listaCarrito.size();
        /* for(int i = 0 ; i < 10; i++)
             System.out.println(p.getNombre());*/
@@ -65,4 +89,26 @@ public class ControladorCarrito {
         model = sUsuario.actualizarEstados(model);
         return "carrito";   
     }
+
+    @PostMapping("/eliminarDelCarrito")
+    public void eliminarDelCarrito(@RequestParam int idp){
+        for(int i = 0; i < listaCarrito.size(); i++){
+            if(listaCarrito.get(i).getId() == idp)
+                listaCarrito.remove(i);
+        }
+        cantidadCarrito = listaCarrito.size();
+    }
+
+    @PostMapping("/actualizarCarrito")
+    public void actualizarCarrito(@RequestParam int idp, @RequestParam int cantidad){
+        for(int i = 0; i < listaCarrito.size(); i++){
+            if(listaCarrito.get(i).getId() == idp){
+                listaCarrito.get(i).setCantidad(cantidad);
+                int subTotal = listaCarrito.get(i).getPrecioCompra()*cantidad;
+                listaCarrito.get(i).setSubTotal(subTotal);
+            }
+        }
+    }
+
+    
 }
--- src/main/resources/static/funciones.js
+++ src/main/resources/static/funciones.js
@@ -43,3 +43,54 @@ function loguearse(){
         }
     });
 }
+
+$(document).ready(function(){
+    $("tr #btnDelete").click(function (){
+    
+        var idp = $(this).parent().find("#idp").val();
+        swal({
+            title: "¿Está seguro de eliminar?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((OK) => {
+                if (OK) {
+                    eliminar(idp);
+                    swal("El producto ha sido eliminado", {
+                        icon: "success",
+                    }).then((ok)=>{
+                        if(ok){
+                            parent.location.href="carrito";
+                        }
+                    });
+                } else {
+                }
+            });
+    });
+
+    function eliminar(idp){
+        var url="eliminarDelCarrito";
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: "idp="+idp,
+            success: function(data, textStatus, jqXHR){
+            }
+        })
+    }
+
+    $("tr #cantidad").change(function(){
+        var idp = $(this).parent().find("#idpro").val();
+        var cantidad = $(this).parent().find("#cantidad").val();
+        var url = "actualizarCarrito";
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: "idp="+idp+"&cantidad="+cantidad,
+            success: function(data, textStatus, jqXHR){
+            }
+        });
+        
+        parent.location.href="carrito";
+    })
+});
