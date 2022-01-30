58e95bfaee36794f81d50e276d0a3c5c69fc5c55
Carlos Rios <carlosandresrio33@hotmail.com>
2022-01-30 14:16:54 -0500

    Funciones añadidas al servicio y repositorio de ordenes
    Se agregaron funciones y consultas a los repositorios de orden y detalles_orden

--- src/main/java/com/example/desayunal/repository/RepositorioDetallesOrden.java
+++ src/main/java/com/example/desayunal/repository/RepositorioDetallesOrden.java
@@ -5,8 +5,12 @@ import java.util.List;
 import com.example.desayunal.model.DetallesOrden;
 import com.example.desayunal.model.Orden;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RepositorioDetallesOrden extends CrudRepository<DetallesOrden,Integer>{
     public List<DetallesOrden> findByOrdenID(Orden usuario);
+
+    @Query(value = "SELECT SUM(cantidad_producto) FROM (SELECT * FROM detalles_orden WHERE fk_ordenid = ?2 AND fk_productoid = ?1)")
+    public int timesSoldProductByOrdenId(int idProducto, int idOrden);
 }
--- src/main/java/com/example/desayunal/repository/RepositorioOrden.java
+++ src/main/java/com/example/desayunal/repository/RepositorioOrden.java
@@ -5,10 +5,17 @@ import java.util.List;
 import com.example.desayunal.model.Orden;
 import com.example.desayunal.model.Usuario;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepositorioOrden extends CrudRepository<Orden,Integer>{
     public List<Orden> findByUsuario(Usuario usuario);
+
+    @Query(value = "SELECT p.id FROM orden WHERE p.mes = ?1")
+    public List<Integer> idsByMes(int mes);
+
+    @Query(value = "SELECT p.id FROM orden WHERE p.dia = ?1 AND p.mes = ?2 AND p.año = ?3")
+    public List<Integer> idsByFecha(int dia, int mes, int año);
 }
