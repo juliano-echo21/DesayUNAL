1ab06e2ba22dca23bef49ef912f780ed73a8bef4
Julian ro <davidro2121@gmail.com>
2022-02-04 20:20:21 -0500

    solución de conflictos

--- src/main/java/com/example/desayunal/repository/RepositorioOrden.java
+++ src/main/java/com/example/desayunal/repository/RepositorioOrden.java
@@ -19,18 +19,12 @@ public interface RepositorioOrden extends CrudRepository<Orden,Integer>{
     
     @Query(value = "SELECT COUNT(id), fk_usuarioid FROM orden GROUP BY fk_usuarioid ORDER BY COUNT(id) DESC LIMIT 0, 5;", nativeQuery = true)
     public List<Integer[]>  usuariosMasFrecuentes();
-<<<<<<< HEAD
     
     @Query(value = "SELECT id FROM Orden WHERE dia = ?1 AND mes = ?2 AND año = ?3")
     public List<Orden> idsByFecha(int dia, int mes, int año);
     
     // Suma de ventas en un mes de un año
-    @Query(value = "SELECT SUM(precio) FROM orden WHERE anio = ?1 AND mes =?2 ;", nativeQuery = true)
+    @Query(value = "SELECT SUM(precio) FROM orden WHERE anio = ?1 AND mes =?2;", nativeQuery = true)
     public Integer ventasMes(int anio, int mes);
 
 }
-=======
-
-  
-}
->>>>>>> 11a004ca56ebf9da5e29168cc935dd490898a48e
